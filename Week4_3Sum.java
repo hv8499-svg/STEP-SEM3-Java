35452846d9cfe9d78723c593a538b9d00b24c062
hv8499-svg <hv8499@srmist.edu.in>
2026-09-11 22:11:17 +0530

    Add STEP SEM-3 Java

---
+++ Week4_3Sum.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Week4_3Sum {
+    static int[][] threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                long sum = (long) nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(new int[]{nums[i], nums[left], nums[right]});
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        int[][] answer = new int[result.size()][3];
+
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[][] result = threeSum(nums);
+
+        for (int[] triplet : result) {
+            System.out.println(Arrays.toString(triplet));
+        }
+    }
+}
